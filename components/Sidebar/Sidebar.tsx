ba779fdaf48342e184bab5d938d046bdcce7e8b2
Julio Ucharima <julio_ucharima@usmp.pe>
2024-07-04 11:47:17 -0500

    add sidebar component

---
+++ components/Sidebar/Sidebar.tsx
@@ -0,0 +1,14 @@
+import { SidebarRoutes } from "@/components/SidebarRoutes";
+import { Logo } from "@/components/Logo";
+
+export const Sidebar = () => {
+  return (
+    <div className="h-screen">
+      <div className="h-full flex flex-col">
+        <Logo />
+
+        <SidebarRoutes />
+      </div>
+    </div>
+  );
+};
