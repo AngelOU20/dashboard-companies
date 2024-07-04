8e24b7a2bd161cd5d6b49f13aec2d9f0f179ec07
Julio Ucharima <julio_ucharima@usmp.pe>
2024-07-04 11:46:26 -0500

    add toogle theme to navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,7 +1,9 @@
+import { Menu, Search } from "lucide-react";
+import { UserButton } from "@clerk/nextjs";
 import { Input } from "@/components/ui/input";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import { UserButton } from "@clerk/nextjs";
-import { Menu, Search } from "lucide-react";
+import { SidebarRoutes } from "@/components/SidebarRoutes";
+import { ModeToggle } from "@/components/ToogleTheme";
 
 export const Navbar = () => {
   return (
@@ -12,7 +14,7 @@ export const Navbar = () => {
             <Menu />
           </SheetTrigger>
           <SheetContent side={"left"}>
-            <p>Sidebar Routes</p>
+            <SidebarRoutes />
           </SheetContent>
         </Sheet>
       </div>
@@ -20,8 +22,8 @@ export const Navbar = () => {
         <Input placeholder="Search..." className="rounded-lg" />
         <Search strokeWidth={1} className="absolute top-2 right-2" />
       </div>
-      <div className="flex gap-x-2 items-center">
-        <p>ToogleTheme</p>
+      <div className="flex gap-x-2 md:gap-x-8 items-center">
+        <ModeToggle />
         <UserButton />
       </div>
     </div>
