65e5f96decbb510f956869b01f046d1d1030a451
Julio Ucharima <julio_ucharima@usmp.pe>
2024-07-04 12:15:17 -0500

    add border right

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -4,7 +4,7 @@ import { Logo } from "@/components/Logo";
 export const Sidebar = () => {
   return (
     <div className="h-screen">
-      <div className="h-full flex flex-col">
+      <div className="h-full flex flex-col border-r">
         <Logo />
 
         <SidebarRoutes />
