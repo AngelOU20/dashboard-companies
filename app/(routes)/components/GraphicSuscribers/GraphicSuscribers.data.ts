d75484f7142236c7cd3b66308df0cda5157cc272
Julio Ucharima <julio_ucharima@usmp.pe>
2024-07-09 13:22:47 -0500

    add GraphicSuscribers component and  GraphicSuscribers data

---
+++ app/(routes)/components/GraphicSuscribers/GraphicSuscribers.data.ts
@@ -0,0 +1,47 @@
+export const dataGraphics = [
+  {
+    year: "2016",
+    newCustomer: 4000,
+    oldCustomer: 2400,
+  },
+  {
+    year: "2017",
+    newCustomer: 3000,
+    oldCustomer: 1398,
+  },
+  {
+    year: "2018",
+    newCustomer: 2000,
+    oldCustomer: 9800,
+  },
+  {
+    year: "2019",
+    newCustomer: 2780,
+    oldCustomer: 3908,
+  },
+  {
+    year: "2020",
+    newCustomer: 1890,
+    oldCustomer: 4800,
+  },
+  {
+    year: "2021",
+    newCustomer: 2300,
+    oldCustomer: 3800,
+  },
+  {
+    year: "2022",
+    newCustomer: 3490,
+    oldCustomer: 4300,
+  },
+  {
+    year: "2023",
+    newCustomer: 4490,
+    oldCustomer: 5300,
+  },
+  {
+    year: "2024",
+    newCustomer: 4600,
+    oldCustomer: 6300,
+  },
+];
