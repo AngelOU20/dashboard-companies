dacbf84c4a65eca149e8e95d3ffb34f040febfb6
Julio Ucharima <julio_ucharima@usmp.pe>
2024-07-09 16:14:22 -0500

    add TotalSuscribers to page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -3,6 +3,7 @@ import { BookOpenCheck, UserRound, Waypoints } from "lucide-react";
 import { CardSummary } from "./components/CardSummary";
 import { LastCustomers } from "./components/LastCustomers";
 import { SalesDistributor } from "./components/SalesDistributor";
+import { TotalSuscribers } from "./components/TotalSuscribers";
 
 export const dataCardSummary = [
   {
@@ -48,6 +49,11 @@ export default function Home() {
       <div className="grid grid-cols-1 xl:grid-cols-2 gap-y-2 md:gap-x-10 mt-12">
         <LastCustomers />
         <SalesDistributor />
+      </div>
+
+      <div className="flex flex-wrap gap-y-4 md:gap-y-0 mt-12 md:mb-10 justify-between">
+        <TotalSuscribers />
+        <p>list integrations</p>
       </div>
     </section>
   );
