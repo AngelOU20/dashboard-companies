c538ac2727d393b343abb79919ef345005467e36
Julio Ucharima <julio_ucharima@usmp.pe>
2024-07-09 21:32:01 -0500

    update sizes

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -9,10 +9,10 @@ type LayoutProps = {
 const Dashboardlayout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex w-full h-full">
-      <div className="hidden lg:block w-80 h-full xl:fixed">
+      <div className="hidden lg:block w-80 h-full lg:fixed">
         <Sidebar />
       </div>
-      <div className="w-full xl:ml-80">
+      <div className="w-full lg:ml-80">
         <Navbar />
         <div className="p-6 bg-[#fafbfc] dark:bg-secondary">{children}</div>
       </div>
