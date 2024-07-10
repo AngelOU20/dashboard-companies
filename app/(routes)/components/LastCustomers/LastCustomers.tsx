f031a7da492a7d383d57b5486e22d940c6527300
Julio Ucharima <julio_ucharima@usmp.pe>
2024-07-09 21:26:03 -0500

    add margin-bottom to the main div

--- app/(routes)/components/LastCustomers/LastCustomers.tsx
+++ app/(routes)/components/LastCustomers/LastCustomers.tsx
@@ -3,7 +3,7 @@ import { CustomIcon } from "@/components/CustomIcon/CustomIcon";
 import { CustomersTable } from "../CustomersTable/CustomersTable";
 export const LastCustomers = () => {
   return (
-    <div className="shadow-sm bg-background rounded-lg p-5">
+    <div className="mb-4 shadow-sm bg-background rounded-lg p-5">
       <div className="flex gap-x-2 items-center">
         <CustomIcon icon={Building} />
         <p className="text-xl">Last customers</p>
