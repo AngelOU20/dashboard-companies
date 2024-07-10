4b0fd76132104d6abadc950021951bb8a4fccf1f
Julio Ucharima <julio_ucharima@usmp.pe>
2024-07-09 21:27:39 -0500

    remove hover shadow

--- app/(routes)/components/TotalSuscribers/TotalSuscribers.tsx
+++ app/(routes)/components/TotalSuscribers/TotalSuscribers.tsx
@@ -8,7 +8,7 @@ import { dataSuscribers as data } from "./TotalSuscribers.data";
 
 export const TotalSuscribers = () => {
   return (
-    <div className="mb-4 lg:mb-8 shadow-sm bg-background rounded-lg p-5 w-full xl:w-96 hover:shadow-lg transition">
+    <div className="mb-4 lg:mb-8 shadow-sm bg-background rounded-lg p-5 w-full xl:w-96">
       <div className="flex gap-x-2 items-center mb-4">
         <CustomIcon icon={Percent} />
         <p className="text-xl">Total Suscribers</p>
