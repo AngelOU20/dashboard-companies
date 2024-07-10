79f7976f55feb8ac607e867c19013943d1546d8b
Julio Ucharima <julio_ucharima@usmp.pe>
2024-07-09 21:26:53 -0500

    add ListIntegrations component and export it

---
+++ app/(routes)/components/ListIntegrations/ListIntegrations.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { List } from "lucide-react";
+import { CustomIcon } from "@/components/CustomIcon";
+import { TableIntegrations } from "../TableIntegrations/";
+
+export const ListIntegrations = () => {
+  return (
+    <div className="shadow-sm bg-background rounded-lg p-5 flex-1">
+      <div className="flex gap-x-2 items-center">
+        <CustomIcon icon={List} />
+        <p className="text-xl">List of integrations</p>
+      </div>
+      <TableIntegrations />
+    </div>
+  );
+};
