a92f626befbee4e1adfcd4045f02472b6300d1ab
Julio Ucharima <julio_ucharima@usmp.pe>
2024-07-13 18:19:12 -0500

    add companies page

---
+++ app/(routes)/companies/page.tsx
@@ -0,0 +1,9 @@
+import { HeaderCompanies } from "./components/HeaderCompanies/HeaderCompanies";
+
+export default function CompaniesPage() {
+  return (
+    <div>
+      <HeaderCompanies />
+    </div>
+  );
+}
