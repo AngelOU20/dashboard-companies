8f9979a9bb0576feec5e4481f4fae86c57c005ad
Julio Ucharima <julio_ucharima@usmp.pe>
2024-07-16 13:22:34 -0500

    add List companies component

---
+++ app/(routes)/companies/components/ListCompanies/ListCompanies.tsx
@@ -0,0 +1,27 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { DataTable } from "./data-table";
+import { columns } from "./columns";
+
+export const ListCompanies = async () => {
+  const { userId } = auth();
+
+  if (!userId) return redirect("/");
+
+  const companies = await db.company.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return (
+    <>
+      <DataTable columns={columns} data={companies} />
+    </>
+  );
+};
