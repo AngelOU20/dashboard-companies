606023081ec09ab3508c627f8e5dc9fb98f57034
Julio Ucharima <julio_ucharima@usmp.pe>
2024-07-20 15:33:45 -0500

    add Calendar component

---
+++ app/(routes)/tasks/components/Calendar/Calendar.tsx
@@ -0,0 +1,160 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import FullCalendar from "@fullcalendar/react";
+import multiMonthPlugin from "@fullcalendar/multimonth";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
+import { DateSelectArg, EventContentArg } from "@fullcalendar/core/index.js";
+
+import { useRouter } from "next/navigation";
+import { formatDate } from "@/lib/formatDate";
+import { toast } from "@/components/ui/use-toast";
+import { CalendarProps } from "./Calendar.type";
+import { ModalAddEvent } from "../ModalAddEvent/ModalAddEvent";
+
+export const Calendar: React.FC<CalendarProps> = ({ companies, events }) => {
+  const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
+  const [onSaveNewEvent, setOnSaveNewEvent] = useState(false);
+  const [selectedItem, setSelectedItem] = useState<DateSelectArg>();
+  const [newEvent, setNewEvent] = useState({
+    eventName: "",
+    companieSelected: {
+      name: "",
+      id: "",
+    },
+  });
+
+  const handleDateClick = async (selected: DateSelectArg) => {
+    setIsOpen(true);
+    setSelectedItem(selected);
+  };
+
+  useEffect(() => {
+    if (onSaveNewEvent && selectedItem?.view.calendar) {
+      const calendarApi = selectedItem.view.calendar;
+      calendarApi.unselect();
+
+      const newEventPrisma = {
+        companyId: newEvent.companieSelected.id,
+        title: newEvent.eventName,
+        start: new Date(selectedItem.start),
+        allDay: false,
+        timeFormat: "H(:mm)",
+      };
+
+      axios
+        .post(
+          `/api/company/${newEvent.companieSelected.id}/event`,
+          newEventPrisma
+        )
+        .then(() => {
+          toast({ title: "Event created!" });
+          router.refresh();
+        })
+        .catch((error) => {
+          toast({
+            title: "Error when creating the event!",
+            variant: "destructive",
+          });
+        });
+
+      setNewEvent({
+        eventName: "",
+        companieSelected: {
+          name: "",
+          id: "",
+        },
+      });
+
+      setOnSaveNewEvent(false);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [onSaveNewEvent, selectedItem, events]);
+
+  const handleEventClick = async (selected: any) => {
+    if (
+      window.confirm(
+        `Are you sure you want to delete this event ${selected.event.title}`
+      )
+    ) {
+      try {
+        await axios.delete(`/api/event/${selected.event._def.publicId}`);
+        toast({ title: "Event deleted!" });
+        router.refresh();
+      } catch (error) {
+        toast({
+          title: "Something went wrong!",
+          variant: "destructive",
+        });
+      }
+    }
+  };
+
+  return (
+    <div>
+      <div className="md:flex gap-x-3">
+        <div className="w-[200px] relative">
+          <div className="absolute top-0 left-0 w-full h-full overflow-auto">
+            <p className="mb-3 text-xl">Listado de tareas</p>
+            {events.map((currentEvent) => (
+              <React.Fragment key={currentEvent.id}>
+                <div className="p-4 mb-2 rounded-lg shadow-sm bg-slate-100">
+                  <p className="font-bold">{currentEvent.title}</p>
+                  <p>{formatDate(currentEvent.start)}</p>
+                </div>
+              </React.Fragment>
+            ))}
+          </div>
+        </div>
+        <div className="flex-1 calendar-container">
+          <FullCalendar
+            plugins={[
+              dayGridPlugin,
+              timeGridPlugin,
+              interactionPlugin,
+              listPlugin,
+              multiMonthPlugin,
+            ]}
+            headerToolbar={{
+              left: "prev,next today",
+              center: "title",
+              right:
+                "timeGridDay,timeGridWeek,dayGridMonth,multiMonthYear,listMonth",
+            }}
+            height={"80vh"}
+            initialView="dayGridMonth"
+            weekends={false}
+            events={events}
+            eventContent={renderEventContent}
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            select={handleDateClick}
+            eventClick={handleEventClick}
+          />
+        </div>
+      </div>
+      <ModalAddEvent
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        setOnSaveNewEvent={setOnSaveNewEvent}
+        companies={companies}
+        setNewEvent={setNewEvent}
+      />
+    </div>
+  );
+};
+
+function renderEventContent(eventInfo: EventContentArg) {
+  return (
+    <div className="bg-slate-200 dark:bg-background w-full p-1">
+      <p className="text-wrap">{eventInfo.event.title}</p>
+    </div>
+  );
+}
