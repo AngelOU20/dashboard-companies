9569cfb9dfca9a2c364e09b09d6d4de43cc32cf1
Julio Ucharima <julio_ucharima@usmp.pe>
2024-07-20 15:37:05 -0500

    define ModalAddEventProps type

---
+++ app/(routes)/tasks/components/ModalAddEvent/ModalAddEvent.type.ts
@@ -0,0 +1,13 @@
+import { Company } from "@prisma/client";
+import { Dispatch, SetStateAction } from "react";
+
+export type ModalAddEventProps = {
+  isOpen: boolean,
+  setIsOpen: Dispatch<SetStateAction<boolean>>,
+  setOnSaveNewEvent: Dispatch<SetStateAction<boolean>>,
+  companies: Company[],
+  setNewEvent: Dispatch<SetStateAction<{
+    eventName: string,
+    companieSelected: { name: string, id: string; },
+  }>>,
+};
