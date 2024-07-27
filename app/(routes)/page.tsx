8132617e827da3dd3e9370f39f80d97426d7d5f0
Julio Ucharima <julio_ucharima@usmp.pe>
2024-07-27 15:21:05 -0500

    update title

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -9,7 +9,7 @@ import { dataCardSummary } from "./components/CardSummary/CardSummary.data";
 export default function Home() {
   return (
     <section>
-      <h2 className="text-2xl mb-4">Dashboard</h2>
+      <h2 className="text-2xl mb-4">All Dashboard</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-x-20">
         {dataCardSummary.map((item) => (
           <React.Fragment key={item.title}>
