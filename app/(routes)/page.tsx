23b5491308c42ce6a36d1926f7f4a51b0f644f0c
Julio Ucharima <julio_ucharima@usmp.pe>
2024-07-27 15:11:29 -0500

    add CardSummary

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -3,14 +3,14 @@ import { LastCustomers } from "./components/LastCustomers";
 import { SalesDistributor } from "./components/SalesDistributor";
 import { TotalSuscribers } from "./components/TotalSuscribers";
 import { ListIntegrations } from "./components/ListIntegrations";
-// import { CardSummary } from "./components/CardSummary";
-// import { dataCardSummary } from "./components/CardSummary/CardSummary.data";
+import { CardSummary } from "./components/CardSummary";
+import { dataCardSummary } from "./components/CardSummary/CardSummary.data";
 
 export default function Home() {
   return (
     <section>
       <h2 className="text-2xl mb-4">Dashboard</h2>
-      {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-x-20">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-x-20">
         {dataCardSummary.map((item) => (
           <React.Fragment key={item.title}>
             <CardSummary
@@ -22,7 +22,7 @@ export default function Home() {
             />
           </React.Fragment>
         ))}
-      </div> */}
+      </div>
 
       <div className="grid grid-cols-1 xl:grid-cols-2 gap-y-4 md:gap-x-10 mt-12">
         <LastCustomers />
